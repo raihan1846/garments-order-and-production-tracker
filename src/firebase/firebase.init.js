140e4ec3ff66c571228825dd1eacbd8161f5e488
raihan1846 <raihan.bd2022@gmail.com>
2025-12-22 16:17:37 +0600

    project start

---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDRTHPun3M_XrTzFWo-ULKyntA1E-rgdoM",
+  authDomain: "garments-order-tracker-94f90.firebaseapp.com",
+  projectId: "garments-order-tracker-94f90",
+  storageBucket: "garments-order-tracker-94f90.firebasestorage.app",
+  messagingSenderId: "745687832054",
+  appId: "1:745687832054:web:aa923fd0925cc35b7ee3d7"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
